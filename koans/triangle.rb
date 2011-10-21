7646342b4bd04db11ec2591f84cf4c16fd7fb4f3
Łukasz Horonziak <hormon@VAIO.localdomain>
2011-10-22 01:27:28 +0200

    Add method: Tringle inequality | Tringle Type

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+   if !((a-b).abs < c && c < a + b)
+      raise TriangleError
+   end  
+
+   if a == b && b == c then
+      :equilateral
+   elsif a == b || b == c || a == c then
+      :isosceles
+   else
+      :scalene
+   end 
 end
 
 # Error class used in part 2.  No need to change this code.
